07cf0ad0632f395e2b86103d0ac2c1579dabc8d0
Nico Janssens <nico.b.janssens@gmail.com>
2015-08-18 10:11:55 +0200

    Updating index.js file -> still dummy.

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ var config = require('config');
 var KadServer = require('./src/kadserver');
 
 var server = KadServer(config);
-// server.on('ready', function () {
-//   server.put('ping', 'pong', 3600)
-//   .then(function() {
-//     console.log('done');
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
-// });
+  server.on('ready', function () {
+    server.put('ping', 'pong', 3600)
+    .then(function() {
+      console.log('ping');
+      return server.get('ping');
+    })
+    .then(function(value) {
+      console.log(value);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 server.on('no peers', function() {
   console.log('no peers');
 })
