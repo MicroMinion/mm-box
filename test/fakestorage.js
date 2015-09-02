61f6e82d3075359d8d9c338b18c3b8ba93120fc5
Nico Janssens <nico.b.janssens@gmail.com>
2015-09-02 15:53:26 +0200

    adding more log info

--- test/fakestorage.js
+++ test/fakestorage.js
@@ -2,21 +2,26 @@ module.exports = FakeStorage;
 
 var EventEmitter = require('events').EventEmitter;
 
-function FakeStorage() {
+function FakeStorage(id) {
   this.data = {};
+  this.id = id;
+  console.log('Creating fakestorage ' + this.id);
 }
 
 FakeStorage.prototype.get = function(key, cb) {
+  console.log('[fakestorage ' + this.id + '] get [' + key + ']');
   if (!this.data[key]) return cb(new Error('not found'));
   cb(null, this.data[key]);
 };
 
 FakeStorage.prototype.put = function(key, val, cb) {
+  console.log('[fakestorage ' + this.id + '] put [' + key + ', ' + val +']');
   this.data[key] = val;
   cb(null, this.data[key]);
 };
 
 FakeStorage.prototype.del = function(key, cb) {
+  console.log('[fakestorage ' + this.id + '] del [' + key + ']');
   delete this.data[key];
   cb(null);
 };
