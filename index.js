864b4deb4373f692e2ef35c5c3d646ee73d467c4
Nico Janssens <nico.b.janssens@gmail.com>
2015-09-04 10:27:36 +0200

    Updating config files -- which revealed another bug ...

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var KadServer = require('./src/kadserver');
 var server = KadServer(config);
   server.on('ready', function () {
     console.log('ready');
-    // server.put('ping', 'pong', 3600)
-    // .then(function() {
-    //   console.log('ping');
-    //   return server.get('ping');
-    // })
-    // .then(function(value) {
-    //   console.log(value);
-    // })
-    // .catch(function (error) {
-    //   console.error(error);
-    // });
+    server.put('ping', 'pong', 1000000000)
+    .then(function() {
+      console.log('ping');
+      return server.get('ping');
+    })
+    .then(function(value) {
+      console.log(value);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 server.on('no peers', function() {
   console.log('no peers');
