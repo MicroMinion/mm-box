5d711aa68a5888db61a6e540df99364969ff9135
Nico Janssens <nico.nj.janssens@alcatel-lucent.com>
2015-09-29 12:02:00 +0200

    simple script to clean nat tables based on description regex

---
+++ src/nat-clean.js
@@ -0,0 +1,26 @@
+var argv = require('minimist')(process.argv.slice(2))
+var nat = require('./nat')
+var Q = require('q')
+
+if (!argv.d) {
+  console.log('Please specify nat description regex using -d')
+  process.exit()
+}
+
+var regex = argv.d
+
+nat.getPortMappingsP()
+  .then(function (portMappings) {
+    var promises = portMappings.map(function (portMapping) {
+      if (portMapping.description.match(regex)) {
+        return nat.unmapPrivateToPublicPortP(portMapping)
+      }
+    })
+    return Q.all(promises)
+  })
+  .catch(function (error) {
+    console.error('Failed to remove all nat portmappings matching description ' + regex + '. ' + error)
+  })
+  .done(function () {
+    console.log('Finished removing all nat portmappings matching description ' + regex)
+  })
