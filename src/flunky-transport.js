9efa18a408f946058816d1427d2e160c16590378
Thomas Delaet <thomas@delaet.org>
2016-01-02 19:34:28 +0100

    Add Flunky Transport (KAD backend)

---
+++ src/flunky-transport.js
@@ -0,0 +1,54 @@
+var debugTransport = require('debug')('flunky-platform:services:kademlia:transport')
+
+/* KADEMLIA CONTACT */
+
+var FlunkyContact = function (options) {
+  this.publicKey = options.publicKey
+  this.connectionInfo = options.connectionInfo
+  kademlia.Contact.call(this, options)
+}
+
+inherits(FlunkyContact, kademlia.Contact)
+
+FlunkyContact.prototype._createNodeID = function () {
+  return kademlia.utils.createID(this.publicKey)
+}
+
+FlunkyContact.prototype.toString = function () {
+  return this.publicKey
+}
+
+/* KADEMLIA TRANSPORT */
+
+var FlunkyTransport = function (options) {
+  debugTransport('initialize FlunkyTransport')
+  this.messaging = options.messaging
+  kademlia.RPC.call(this, options)
+  var self = this
+  process.nextTick(function () {
+    self.emit('ready')
+  })
+  this.messaging.on('self.kademlia', this._onMessage.bind(this))
+  this.messaging.on('friends.kademlia', this._onMessage.bind(this))
+  this.messaging.on('public.kademlia', this._onMessage.bind(this))
+}
+
+FlunkyTransport.prototype._onMessage = function (topic, publicKey, data) {
+  debugTransport('_onMessage')
+  data = new Buffer(JSON.stringify(data), 'utf8')
+  this._handleMessage(data, {publicKey: publicKey})
+}
+
+FlunkyTransport.prototype._createContact = function (options) {
+  return new FlunkyContact(options)
+}
+
+FlunkyTransport.prototype._send = function (data, contact) {
+  debugTransport('_send')
+  data = JSON.parse(data.toString('utf8'))
+  this.messaging.send('kademlia', contact.publicKey, data, {realtime: true, expireAfter: 10000})
+}
+
+FlunkyTransport.prototype._close = function () {}
+
+inherits(FlunkyTransport, kademlia.RPC)
