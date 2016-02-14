3d9bc6d256bf242aabd0ea773e6d3ebf6a40b8aa
Thomas Delaet <thomas@delaet.org>
2016-02-14 15:30:20 +0100

    Temporary disable classes that have not been implemented proplery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  BootstrapNodes: require('./bootstrap-nodes.js'),
-  StandaloneDHT: require('./standalone.dht'),
   services: require('./services/index.js')
 }
+
+// BootstrapNodes: require('./bootstrap-nodes.js'),
+// StandaloneDHT: require('./standalone.dht'),
