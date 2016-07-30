27fa00388e05684a3646a7172b0abe61ac6dc296
Thomas Delaet <thomas@delaet.org>
2016-07-30 10:36:55 +0200

    formatting

--- index.js
+++ index.js
@@ -6,64 +6,65 @@ var Kademlia = require('mm-services-kademlia')
 var winston = require('winston')
 var MemStore = require('kad-memstore-thomas')
 var winstonWrapper = require('winston-meta-wrapper')
+var _ = require('lodash')
 
 /**
  * Create Standalone DHT node
  */
-function DHT(options) {
-    var self = this
-    if (!(this instanceof DHT)) return new DHT()
-    if (!options) {
-        options = {}
-    }
-    if (options.logger) {
-        this._logger = options.logger
-    } else {
-        this._logger = winston
-    }
-    if (!options.mDNS) {
-        options.mDNS = true
-    }
-    if (!options.platformStore) {
-        this.platformStore = new MemStore()
-    } else {
-        this.platformStore = options.platformStore
-    }
-    if (!options.dhtStore) {
-        this.dhtStore = new MemStore()
-    } else {
-        this.dhtStore = options.dhtStore
-    }
-    this.platform = new Platform({
-        storage: this.platformStore,
-        connectionInfo: options.connectionInfo,
-        logger: winstonWrapper(this._logger),
-        identity: options.identity
+function DHT (options) {
+  var self = this
+  if (!(this instanceof DHT)) return new DHT()
+  if (!options) {
+    options = {}
+  }
+  if (options.logger) {
+    this._logger = options.logger
+  } else {
+    this._logger = winston
+  }
+  if (_.isUndefined(options.mDNS)) {
+    options.mDNS = true
+  }
+  if (!options.platformStore) {
+    this.platformStore = new MemStore()
+  } else {
+    this.platformStore = options.platformStore
+  }
+  if (!options.dhtStore) {
+    this.dhtStore = new MemStore()
+  } else {
+    this.dhtStore = options.dhtStore
+  }
+  this.platform = new Platform({
+    storage: this.platformStore,
+    connectionInfo: options.connectionInfo,
+    logger: winstonWrapper(this._logger),
+    identity: options.identity
+  })
+  this.platform.on('ready', function () {
+    self._logger = winstonWrapper(self._logger)
+    self._logger.addMeta({
+      node: self.platform.identity.getSignId()
     })
-    this.platform.on('ready', function() {
-        self._logger = winstonWrapper(self._logger)
-        self._logger.addMeta({
-            node: self.platform.identity.getSignId()
-        })
-        self._initializeServices(options.mDNS)
-    })
+    self._initializeServices(options.mDNS)
+  })
 }
 
-DHT.prototype.start = function() {}
+DHT.prototype.start = function () {}
 
-DHT.prototype._initializeServices = function(mDNS) {
-    if (mDNS) {
-        this.mdns = new MulticastDNS({
-            platform: this.platform,
-            logger: this._logger
-        })
-    }
-    this.dht = new Kademlia({
-        platform: this.platform,
-        storage: this.dhtStore,
-        logger: this._logger,
-        seeds: null
+DHT.prototype._initializeServices = function (mDNS) {
+  if (mDNS) {
+    this.mdns = new MulticastDNS({
+      platform: this.platform,
+      logger: this._logger
     })
+  }
+  this.dht = new Kademlia({
+    platform: this.platform,
+    storage: this.dhtStore,
+    logger: this._logger,
+    seeds: null
+  })
 }
 
 module.exports = DHT
