e3cb21a835b474ee6a67045c8f642f2c896b57a7
Thomas Delaet <thomas@delaet.org>
2016-09-03 23:58:02 +0200

    decrease timeout and make variables explicit

--- example/example.js
+++ example/example.js
@@ -2,13 +2,15 @@ var Runtime = require('../index.js')
 
 var peers = {}
 var timeout = 0
-var TIMEOUT_STEP = 5000
+var TIMEOUT_STEP = 1000
 
 var createPeer = function (name) {
   setTimeout(function () {
     var dht = new Runtime({
       SERVICES: "mdns kademlia",
-      LOG_CONTEXT: true
+      LOG_CONTEXT: true,
+      DEBUG_LEVEL: 0,
+      LOGSTASH: true
     })
     dht.createPlatform()
     peers[name] = dht
